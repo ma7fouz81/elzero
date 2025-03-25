1a0a0d3d988ce0914d079bfa32965ec2e5d06af0
ma7fouz81 <amedmafouz53@gmail.com>
2025-03-25 02:41:26 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+
+<?php
+echo 'we love php ';
+echo '<br>';
+ECHO 'we love php ';
+echo '<br>';
+print 'we love php ';
+echo '<br>';
+PRINT 'we love php ';
+?>
+
+<?= 'we love php';?> 
+
+<?php
+echo "my name is ahmed "; //single line code
+echo "my name is ahmed "; #single line code

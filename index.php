ad8b8adbc82cc683e24eb435b871cde13e3be374
ma7fouz81 <amedmafouz53@gmail.com>
2025-03-28 21:22:05 +0200

    Week 1&2

--- index.php
+++ index.php
@@ -1,118 +1,118 @@
 
+<!-- Week 1&2 -->
+
+
 <!-- //data types -->
 <?php
-// echo gettype(true);
-// echo '<br>';
-// echo gettype(false);
-// echo '<br>';
-// echo gettype('Ahmed');
-// echo '<br>';
-// echo gettype(10);
-// echo '<br>';
-// echo gettype(10.5);
-// echo '<br>';
-// echo gettype(["Ahmed","Mahfouz"]);
-// echo '<br>';
-// echo gettype(array("egy" => "egypt"));
-// echo '<br>';
-// echo gettype(array("egypt"));
-########################################
-?>
-<!-- //Type Juggling And Automatic Type Conversion -->
+echo gettype(true);
+echo gettype(false);
+echo '<br>';
+echo gettype('Ahmed');
+echo '<br>';
+echo gettype(10);
+echo '<br>';
+echo gettype(10.5);
+echo '<br>';
+echo gettype(["Ahmed","Mahfouz"]);
+echo '<br>';
+echo gettype(array("egy" => "egypt"));
+echo '<br>';
+echo gettype(array("egypt"));
 
-<?php
 
-// echo true; //1
-// echo '<br>';
-// echo 1+true; //2
-// echo '<br>';
-// echo gettype(1+true); // int
-// echo '<br>';
-// echo 1+"2"; // 3
-// echo '<br>';
-// echo gettype(1+"2"); // int
-// echo '<br>';
-// echo 1+"2 book"; // 3 => warning non numeric 
-// echo '<br>';
-// echo gettype(1+"2 book"); // int =>> warning
-// echo '<br>';
-// echo 10+10.5; //20.5
-// echo '<br>';
-// echo gettype(10+10.5); // double
-?>
+ //Type Juggling And Automatic Type Conversion -->
 
-<!-- type casting -->
 
-<?php
-// echo 1+(int)"2 book"; //3
-// echo '<br>';
-// echo gettype( 1+(int)"2 book"); // int 
-// echo '<br>';
-// echo 10+10.5; //20.5
-// echo '<br>';
-// echo 10+(int)10.5; //20
-// echo '<br>';
-// echo gettype(10+(int)10.5); // int
-// echo '<br>';
-// echo 10.5+10.5; // 21 
-// echo '<br>';
-// echo gettype(10.5+10.5); //double
-// echo '<br>';
-// echo (int)(10.5+10.5); //21
-// echo '<br>';
-// echo gettype((int)(10.5+10.5)); // int
-?>
 
-<!-- Boolean And Converting To Boolean -->
-<?php
-// var_dump((bool) 0);
-// echo "<br>";
-// var_dump((bool) array());
-// echo "<br>";
-// var_dump((bool) []);
-// echo "<br>";
-// var_dump((bool) "");
-// echo "<br>";
-// var_dump((bool) "0");
-// echo "<br>";
-// var_dump((bool) 1);
-// echo "<br>";
-// var_dump((bool) "Ahmed");
-// echo "<br>";
-// var_dump((bool) [1]);
-// echo "<br>";
-// var_dump((bool) 10);
-// echo "<br>";
-// var_dump((bool) -10);
+echo true; //1
+echo '<br>';
+echo 1+true; //2
+echo '<br>';
+echo gettype(1+true); // int
+echo '<br>';
+echo 1+"2"; // 3
+echo '<br>';
+echo gettype(1+"2"); // int
+echo '<br>';
+echo 1+"2 book"; // 3 => warning non numeric 
+echo '<br>';
+echo gettype(1+"2 book"); // int =>> warning
+echo '<br>';
+echo 10+10.5; //20.5
+echo '<br>';
+echo gettype(10+10.5); // double
 
-?>
 
-<!-- String And Escaping -->
-<?php
-// echo "'PHP'";
-// echo '<br>';
-// echo '"PHP"';
-// echo '<br>';
-// echo '\'PHP\'';
-// echo '<br>';
-// echo 'PHP\\';
-// echo '<br>';
-// echo 'PHP on
-// Multible
-// Lines';
-// echo '<br>';
-// echo nl2br('PHP on
-// Multible
-// Lines');
-?>
+// <!-- type casting -->
 
-<!-- Heredoc And Nowdoc -->
 
-<?php
+echo 1+(int)"2 book"; //3
+echo '<br>';
+echo gettype( 1+(int)"2 book"); // int 
+echo '<br>';
+echo 10+10.5; //20.5
+echo '<br>';
+echo 10+(int)10.5; //20
+echo '<br>';
+echo gettype(10+(int)10.5); // int
+echo '<br>';
+echo 10.5+10.5; // 21 
+echo '<br>';
+echo gettype(10.5+10.5); //double
+echo '<br>';
+echo (int)(10.5+10.5); //21
+echo '<br>';
+echo gettype((int)(10.5+10.5)); // int
+
+// <!-- Boolean And Converting To Boolean -->
+
+var_dump((bool) 0);
+echo "<br>";
+var_dump((bool) array());
+echo "<br>";
+var_dump((bool) []);
+echo "<br>";
+var_dump((bool) "");
+echo "<br>";
+var_dump((bool) "0");
+echo "<br>";
+var_dump((bool) 1);
+echo "<br>";
+var_dump((bool) "Ahmed");
+echo "<br>";
+var_dump((bool) [1]);
+echo "<br>";
+var_dump((bool) 10);
+echo "<br>";
+var_dump((bool) -10);
+
+
+
+// <!-- String And Escaping -->
+
+echo "'PHP'";
+echo '<br>';
+echo '"PHP"';
+echo '<br>';
+echo '\'PHP\'';
+echo '<br>';
+echo 'PHP\\';
+echo '<br>';
+echo 'PHP on
+Multible
+Lines';
+echo '<br>';
+echo nl2br('PHP on
+Multible
+Lines');
+
+// <!-- Heredoc And Nowdoc -->
+
+
 
 $name = "Ahmed";
 
-Heredoc It analyzes the code
+// Heredoc It analyzes the code
 
 echo <<<"here"
 Hello PHP 
@@ -122,7 +122,7 @@ here;
 
 echo '<br>';
 
-Nowdoc It escaping the code only
+// Nowdoc It escaping the code only
 
 echo <<<'Now'
 Hello PHP 
@@ -145,4 +145,18 @@ echo <<<"ullinks"
     <li> $name</li>
 </ul>
 ullinks;
-?>
+echo"///////////////////////////////////////////////////////////////////////";
+
+echo '<pre>';
+print_r([
+
+    "0" => "Ahmed",
+    "A" => "Ali",
+    "B" => "Basem",
+    true =>"Amr",
+    false => "Mohammed",
+    "Eman",
+    "Mahmoud",
+    "Names"=>["osama","osman","omar"]
+]);
+echo '</pre>';

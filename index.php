030d04d1f484b4e5dc51d078faec6c4a459a777b
ma7fouz81 <amedmafouz53@gmail.com>
2025-04-01 12:08:59 +0200

    week 4

--- index.php
+++ index.php
@@ -1,90 +1,323 @@
-<!-- Week 3 -->
+<!-- Week 4 -->
  
 <?php
-##variables
-$username= "Ahmed";
-$userName= "Mahfouz";
-echo "Hello $username";
+//Arithmetic Operators
+
+//1-addition
+
+echo 10+20;
 echo '<br>';
-echo "Hello $userName";
+echo gettype(10+20);
 echo '<br>';
-echo "//////////////////////////////////";
+echo 9.5+20.5;
 echo '<br>';
-//variable variable
-$a="Ahmed";
-$$a="Mahfouz";
-$$$a="Saad";
+echo gettype(9.5+20.5);
+echo '<br>';
+//2-subtraction
+
+echo 10-20;
+echo '<br>';
+echo gettype(10-20);
+echo '<br>';
+echo 9.5-20.5;
+echo '<br>';
+echo gettype(9.5-20.5);
+echo '<br>';
+//3-muliplication
+
+echo 10*20;
+echo '<br>';
+echo gettype(10*20);
+echo '<br>';
+echo 9.5*20.5;
+echo '<br>';
+echo gettype(9.5*20.5);
+echo '<br>';
+
+//4-Division
+
+echo 20 / 10;
+echo '<br>';
+echo gettype( 20 / 10);
+echo '<br>';
+echo 20 / 8;
+echo '<br>';
+echo gettype(20 / 8);
+echo '<br>';
+
+//5-modulus
+
+echo 20 % 10;
+echo '<br>';
+echo gettype( 20 % 10);
+echo '<br>';
+echo 21 % 10;
+echo '<br>';
+echo gettype(21%10);
+echo '<br>';
+echo 25 % 10;
+echo '<br>';
+echo gettype(25%10);
+echo '<br>';
+
+//6-exponentiation
+
+echo 2 ** 4;
+echo '<br>';
+echo gettype( 2 ** 4);
+echo '<br>';
+echo 2 * 2 * 2 * 2;
+echo '<br>';
+echo gettype(2 * 2 * 2 * 2);
+echo '<br>';
+echo 2 ** 5;
+echo '<br>';
+echo gettype(2 ** 5);
+echo '<br>';
+
+//7-identity
+
+echo "100";
+echo '<br>';
+echo gettype("100");
+echo '<br>';
+echo +"100";
+echo '<br>';
+echo gettype(+"100");
+echo '<br>';
+
+//8-negation
+
+echo "100";
+echo '<br>';
+echo gettype("100");
+echo '<br>';
+echo -"100";
+echo '<br>';
+echo gettype(-"100");
+echo '<br>';
+
+echo "-100";
+echo '<br>';
+echo gettype("100");
+echo '<br>';
+echo -"-100";
+echo '<br>';
+echo gettype(-"-100");
+echo '<br>';
+echo "*******************************************";
+echo '<br>';
+?>
+
+<?php
+//1- Assignment Operators
+$a = 10;
+// $a = $a + 20;
+$a += 20;
+
+$b = 20;
+// $b = $b - 5;
+$b -= 5;
+
+$c = 4;
+// $c = $c ** 4;
+$c **= 4;
+
 echo $a;
 echo '<br>';
-echo $$a;
+echo $b;
 echo '<br>';
-echo $Ahmed;
+echo $c;
 echo '<br>';
-echo $$$a;
+echo "******************";
 echo '<br>';
-echo $Mahfouz;
+
+//2- Comparison Operators
+
+//1- equal =>> بيقارن قيمه فقط
+
+var_dump(100==100);
 echo '<br>';
-echo "Hello $a";
+var_dump(100=="100");
 echo '<br>';
-echo "Hello ${$a}";
+var_dump(100.0==100);
 echo '<br>';
-echo "Hello ${$$a}";
+var_dump(100 != 100);
 echo '<br>';
-echo "//////////////////////////////////";
+echo "******************";
 echo '<br>';
-// Assign By Value And By Reference
+
+//2- identical =>> بيقارن قيمه ونوع
+
+var_dump(100 === 100);
+echo '<br>';
+var_dump(100 === "100");
+echo '<br>';
+var_dump(100.0 === 100);
+echo '<br>';
+var_dump(100 !== "100");
+
+echo '<br>';
+echo "******************";
+echo '<br>';
+
+var_dump(100 >100);
+echo '<br>';
+var_dump(100 == 100);
+echo '<br>';
+var_dump(100 >= 100);
+echo '<br>';
+var_dump(100 >= 10);
+echo '<br>';
+var_dump(10 < 50);
+echo '<br>';
+var_dump(100 <=> 10); // 1
+echo '<br>';
+var_dump(10 <=> 10); // 0
+echo '<br>';
+var_dump(10 <=> 100); // -1
+
+echo '<br>';
+echo "******************";
+echo '<br>';
+
+//3-  Increment And Decrement Operators
+
+$a=0;
+
+echo $a++; //0 
+echo '<br>';
+echo $a; // 1
+echo '<br>';
+
+$b=0;
+echo $b--; //0
+echo '<br>';
+echo $b; //-1 
+echo '<br>';
+
+$c=0;
+
+echo ++$c; //1
+echo '<br>';
+echo $c; // 1
+echo '<br>';
+
+$d=0;
+echo --$d; //-1
+echo '<br>';
+echo $d; //-1 
+
+echo '<br>';
+echo "******************";
+echo '<br>';
+
+//4-  Logical Operators
+
+var_dump(100 > 50 and 50 > 10 and 10 > 5);
+echo '<br>';
+var_dump(100 > 50 or 50 < 10 or 10 < 5);
+echo '<br>';
+// xor =>> one is true but not both
+var_dump(100 > 50 xor 10 < 5); 
+echo '<br>';
+var_dump(100 > 50 xor 10 > 5);
+
+echo '<br>';
+echo "******************";
+echo '<br>';
+
+// 5- String Operators[concatination]
 
 $a="Ahmed";
 $b="Mahfouz";
-$b=$a;
-$b="Saad";
 
-echo $a; // Ahmed
+echo "$a $b";
 echo '<br>';
-echo $b; // Saad
+echo $a. " " .$b;
 echo '<br>';
+echo "{$a} {$b}";
 
-echo "//////////////////////////////////";
 echo '<br>';
-$a="Ahmed";
-$b="Mahfouz";
-$b=&$a;
-$b="Saad";
-
-echo $a; // Saad
-echo '<br>';
-echo $b; // Saad
+echo "******************";
 echo '<br>';
 
-echo "//////////////////////////////////";
-echo '<br>';
-// Introduction To Constants
+//6-Array Operators
 
-define("DB_NAME","elzero");
-echo DB_NAME;
+//1- equal & not equal [same key & value  without ordering]
+$arr1=[1 => "1", 2 => "2"];
+$arr2=[3 => "3", 4 => "4"];
+$arr3=$arr1 + $arr2;
+
+echo "<pre>";
+print_r($arr1 + $arr2);
 echo '<br>';
-define("MAIN_NUM",10);
-//define("MAIN_NUM",5); // Warning: Constant MAIN_NUM already defined 
-echo MAIN_NUM *10;
+print_r($arr3);
+echo "</pre>";
+
+$arr3=[5 => "10", 6 => "20"];
+$arr4=[6 => "20", 5 => "10"];
+
+var_dump($arr3 == $arr4);
 echo '<br>';
-echo "//////////////////////////////////";
+var_dump($arr3 != $arr4);
+echo '<br>';
+var_dump($arr3 <> $arr4);
+
+echo '<br>';
+echo "******************";
 echo '<br>';
 
-//Predefined And Magic Constants 
+//2- identical [same key & value & typewith ordering]
 
-//1-Predefined Constants[Case sensitive]
-echo PHP_VERSION; //تخزن رقم اصدار الكود  
-echo '<br>';
-echo PHP_OS_FAMILY; // تعرض نوع نظام التشغيل [windows]
-echo '<br>';
-echo PHP_INT_MAX; // عرض أكبر عدد صحيح يمكن تخزينه بناءً على نظام التشغيل 
-echo '<br>';
-echo get_include_path(); // عرض المسارات التي يستخدمها PHP للبحث عن الملفات المضمنة افتراضيًا.
+$arr5=[1 => "10", 2 => "20"];
+$arr6=[1 => "20", 2 => "10"];
+
+var_dump($arr5===$arr6); 
 echo '<br>';
 
-//2-Magic Constants[Case insensitive]
-echo __LINE__; // رقم السطر
+$arr5=[1 => "10", 2 => "20"];
+$arr6=[1 => "10", 2 => "10"];
+var_dump($arr5===$arr6); 
 echo '<br>';
-echo __FILE__; // اقدر اوصل للمكان اللي انا فيه بتاع الملفات
+
+$arr5=[1 => "10", 2 => "20"];
+$arr6=[1 => "10", 2 => "20"];
+var_dump($arr5===$arr6); 
+
 echo '<br>';
-echo __DIR__; 
+echo "******************";
+echo '<br>';
+
+//7-  Operator Precedence(اولويه العمليات الحسابيه)
+
+echo 2+3*6; // 20
+echo '<br>';
+echo (2+3)*6; // 30
+echo '<br>';
+$a= 10 || false; //(10 || false)
+echo $a; // true => 1
+
+echo '<br>';
+$b= 10 or false; // (10) or false
+echo $b; // 10
+
+echo '<br>';
+
+$b= 0 or false; // (0) or false
+echo $b; // 0
+
+//8- Error Control Operator
+
+// $a=10;
+// $b=@$a or die ("variable not found"); // variable not found
+// echo $b; 
+
+
+// $c=10;
+// $b=@$a or die ("variable not found"); // variable not found
+// echo "test $d"; 
+
+// echo '<br>';
+// echo "******************";
+// echo '<br>';
